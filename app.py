28744549f94b95de8bcc79210dea0d585d8a8c6e
Snake <sneider.caicedo@gmail.com>
2022-02-10 05:44:24 -0500

    Create Log-In

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def inicioSesion():
+    return render_template('index.html')
 
+
+"""
+@app.route('/Sing Up')
+def Registro():
+    return 'Registro de Usuarios'
+"""
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
