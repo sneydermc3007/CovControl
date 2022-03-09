5843d9c2d819d5e7ef37e9672421dadd16956f10
Sneyder <sneider.caicedo@gmail.com>
2022-03-09 11:37:59 -0500

    Create template menu 1

---
+++ front-end/src/components/Formulario.vue
@@ -0,0 +1,22 @@
+<template>
+  <html lang="es">
+    <head>
+      <title>CovControl Formulario</title>
+    </head>
+
+    <body>
+
+    </body>
+  </html>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Formulario"
+}
+</script>
+
+<style scoped>
+
+</style>
