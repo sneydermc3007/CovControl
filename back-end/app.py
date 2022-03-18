ee894c729120d781e5699ae1caab067c8ef613d4
Juan José Mazo <63790748+JuanJoseMA2020@users.noreply.github.com>
2022-03-17 23:29:43 -0500

    Last Change
    Cambio en metodo para mejor guardado

--- back-end/app.py
+++ back-end/app.py
@@ -1,42 +1,80 @@
-    from urllib import response
-    from flask import Flask, jsonify, request, Response
-    from flask_pymongo import PyMongo
-    from bson import json_util
-    from bson.objectid import ObjectId
+from urllib import response
+from flask import Flask, jsonify, request, Response
+from flask_pymongo import PyMongo
+from bson import json_util 
+from bson.objectid import ObjectId
+from pymongo import MongoClient
 
-    from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    app.secret_key = 'myawesomesecretkey'
+#app.secret_key = 'myawesomesecretkey'
 
-    app.config['MONGO_URI'] = 'mongodb://database/pythonmongodb'
+#app.config['MONGO_URI'] = 'mongodb://database/pythonmongodb'
 
-    mongo = PyMongo(app)
-
-    #REGISTOR DE USUARIOS U.CATOLICA
-    @app.route('/users', methods=['POST'])
-    def create_user():
-        # Receiving Data
-        username = request.json['username']  #-->ES LA CEDULA
-        email = request.json['email']
-        password = request.json['password']
-        
-        if username and email and password:
-            mongo.db.users.insert(
-                {'username': username, 'email': email, 'password':password}
-            )
-            response = {
-                'username': username,
-                'email': email,
-                'password': password
-            }
-            return response
-        else:
-            return {'message': 'received'}
+#mongo = PyMongo(app)
 
 
+@app.route('/registro', methods = ['POST'])
+def create_user():
+    #Receiving data
+    username = request.json['username'] #---->Esta es la cedula
+    password = request.json['password']
+    email = request.json['email']
+    firstname = request.json['firstname']
+    lastname = request.json['lastname']
+    gender = request.json['gender']
+    number = request.json['number']
+
+    return {'message': 'received'}
 
 
-    if __name__ == "__main__":
+@app.route('/login', methods = ['GET'])
+def login():
+    #Looking for data
+    username = request.json['username'] #---->Esta es la cedula
+    password = request.json['password']
+    email = request.json['email']
+    firstname = request.json['firstname']
+    lastname = request.json['lastname']
+    gender = request.json['gender']
+    number = request.json['number']
+
+    return {'message': 'received'}
+###GET,PUT, DELETE
+
+
+    
+
+""""
+    if username and password and email:
+        hashed_password = generate_password_hash(password)
+        id = mongo.db.users.insert(
+            {'username': username, 'password': hashed_password, 'email': email}
+        )
+        response = {
+            'id': str(id),
+            'username': username,
+            'password': hashed_password,
+            'email': email,
+            #firstname = request.json['firstname]
+            #lastname = request.json['lastname']
+            #gender = request.json['gender]
+            #number = request.json['number']
+        }
+        return response
+
+    else:
+        return {'message': 'received'}
+
+
+
+
+    
+
+
+"""
+
+if __name__ == "__main__":
         app.run(debug=True)
